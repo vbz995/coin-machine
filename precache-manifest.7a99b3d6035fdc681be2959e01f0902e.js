e4e8cfa000d7a807bfc1d0212c21a052e042b65e
Bozidar Vrbarac <vrbaracbozidar@gmail.com>
2019-11-20 09:22:15 +0100

    Updates

---
+++ precache-manifest.7a99b3d6035fdc681be2959e01f0902e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f9d7a21ec6ea5482f6e3e86eba91c33",
+    "url": "/coin-machine/index.html"
+  },
+  {
+    "revision": "c12fc669a0041e99e7e6",
+    "url": "/coin-machine/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "9c96dac57dfb488df43d",
+    "url": "/coin-machine/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "c12fc669a0041e99e7e6",
+    "url": "/coin-machine/static/js/2.6cb2d7f0.chunk.js"
+  },
+  {
+    "revision": "9c96dac57dfb488df43d",
+    "url": "/coin-machine/static/js/main.46ae5dd4.chunk.js"
+  },
+  {
+    "revision": "f7f67984686d22475402",
+    "url": "/coin-machine/static/js/runtime-main.21da84ca.js"
+  },
+  {
+    "revision": "430fa632dec05be0ee1ad3516e8054e5",
+    "url": "/coin-machine/static/media/coins.430fa632.jpg"
+  }
+]);
